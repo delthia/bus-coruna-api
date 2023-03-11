a4601b6cf8701809b21c9ce1c994518c9195711f
delthia <git@delthia.com>
2023-03-11 20:04:50 +0100

    Arreglar simbolo-bus.js

--- transport/static/scripts/simbolo-bus.js
+++ transport/static/scripts/simbolo-bus.js
@@ -21,32 +21,36 @@ function actualizar(last) {
         .then(function(json) {
             document.getElementById('buses-ida').innerHTML = '';
             document.getElementById('buses-vuelta').innerHTML = '';
-            for(i=0;i<json['paradas'][0]['paradas'].length;i++) {
-                // const altura = document.getElementById('ida').offsetHeight-35;
-                const altura = document.getElementById('ida').offsetHeight-70;
-                const x = (json['paradas'][0]['paradas'][i]['buses'][0]['distancia']*altura)+8;
-                if(json['paradas'][0]['paradas'][i]['buses'][0]['estado'] == 0) {
-                    document.getElementById('buses-ida').innerHTML += '<div style="margin-top: '+x+'px; position: absolute"><span class="material-symbols-outlined simbolo_bus simbolo_bus_parada">directions_bus</span></div>';
-                }
-                else if(json['paradas'][0]['paradas'][i]['buses'][0]['estado'] == 1) {
-                    document.getElementById('buses-ida').innerHTML += '<div style="margin-top: '+x+'px; position: absolute"><span class="material-symbols-outlined simbolo_bus">directions_bus</span></div>';
-                }
-                else {
-                    document.getElementById('buses-ida').innerHTML += '<div style="margin-top: '+x+'px; position: absolute"><span class="material-symbols-outlined simbolo_bus simbolo_bus_otro">directions_bus</span></div>';
+            if(json['paradas'][0]['paradas'] != undefined) {
+                for(i=0;i<json['paradas'][0]['paradas'].length;i++) {
+                    // const altura = document.getElementById('ida').offsetHeight-35;
+                    const altura = document.getElementById('ida').offsetHeight-70;
+                    const x = (json['paradas'][0]['paradas'][i]['buses'][0]['distancia']*altura)+8;
+                    if(json['paradas'][0]['paradas'][i]['buses'][0]['estado'] == 0) {
+                        document.getElementById('buses-ida').innerHTML += '<div style="margin-top: '+x+'px; position: absolute"><span class="material-symbols-outlined simbolo_bus simbolo_bus_parada">directions_bus</span></div>';
+                    }
+                    else if(json['paradas'][0]['paradas'][i]['buses'][0]['estado'] == 1) {
+                        document.getElementById('buses-ida').innerHTML += '<div style="margin-top: '+x+'px; position: absolute"><span class="material-symbols-outlined simbolo_bus">directions_bus</span></div>';
+                    }
+                    else {
+                        document.getElementById('buses-ida').innerHTML += '<div style="margin-top: '+x+'px; position: absolute"><span class="material-symbols-outlined simbolo_bus simbolo_bus_otro">directions_bus</span></div>';
+                    }
                 }
             }
-            for(i=0;i<json['paradas'][0]['paradas'].length;i++) {
-                // const altura = document.getElementById('vuelta').offsetHeight-35;
-                const altura = document.getElementById('vuelta').offsetHeight-70;
-                const x = (json['paradas'][1]['paradas'][i]['buses'][0]['distancia']*altura)+8;
-                if(json['paradas'][1]['paradas'][i]['buses'][0]['estado'] == 0) {
-                    document.getElementById('buses-vuelta').innerHTML += '<div style="margin-top: '+x+'px; position: absolute"><span class="material-symbols-outlined simbolo_bus simbolo_bus_parada">directions_bus</span></div>';
-                }
-                else if(json['paradas'][1]['paradas'][i]['buses'][0]['estado'] == 1) {
-                    document.getElementById('buses-vuelta').innerHTML += '<div style="margin-top: '+x+'px; position: absolute"><span class="material-symbols-outlined simbolo_bus">directions_bus</span></div>';
-                }
-                else {
-                    document.getElementById('buses-vuelta').innerHTML += '<div style="margin-top: '+x+'px; position: absolute"><span class="material-symbols-outlined simbolo_bus simbolo_bus_otro">directions_bus</span></div>';
+            if(json['paradas'][1]['paradas'] != undefined) {
+                for(i=0;i<json['paradas'][1]['paradas'].length;i++) {
+                    // const altura = document.getElementById('vuelta').offsetHeight-35;
+                    const altura = document.getElementById('vuelta').offsetHeight-70;
+                    const x = (json['paradas'][1]['paradas'][i]['buses'][0]['distancia']*altura)+8;
+                    if(json['paradas'][1]['paradas'][i]['buses'][0]['estado'] == 0) {
+                        document.getElementById('buses-vuelta').innerHTML += '<div style="margin-top: '+x+'px; position: absolute"><span class="material-symbols-outlined simbolo_bus simbolo_bus_parada">directions_bus</span></div>';
+                    }
+                    else if(json['paradas'][1]['paradas'][i]['buses'][0]['estado'] == 1) {
+                        document.getElementById('buses-vuelta').innerHTML += '<div style="margin-top: '+x+'px; position: absolute"><span class="material-symbols-outlined simbolo_bus">directions_bus</span></div>';
+                    }
+                    else {
+                        document.getElementById('buses-vuelta').innerHTML += '<div style="margin-top: '+x+'px; position: absolute"><span class="material-symbols-outlined simbolo_bus simbolo_bus_otro">directions_bus</span></div>';
+                    }
                 }
             }
             document.getElementById('t').innerHTML = cadenas[idioma][3]+': '+right.toLocaleString(cadenas[idioma][4]);
