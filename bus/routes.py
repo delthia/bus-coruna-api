d372b4caa8e34eb6594d86e1170e171ec7ce1c8c
delthia <git@delthia.com>
2023-08-12 21:40:28 +0200

    Añadir licencia al inicio de los archivos

--- bus/routes.py
+++ bus/routes.py
@@ -1,3 +1,19 @@
+# bus.delthia.com - información en tiempo real del bus de Coruña
+# Copyright (C) 2023  <Iago, delthia@delthia.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from flask import render_template, url_for, request, redirect, send_file
 from bus import app
 from bus.utils import buses_parada, buses_linea, encontrar_linea, encontrar_parada, geojson_linea, salidas #, geojson_buses
