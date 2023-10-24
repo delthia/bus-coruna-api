ab6f3cae78199ac9781982f49f8c444c1428c07f
delthia <git@delthia.com>
2023-10-24 20:35:20 +0200

    Limpiar código de parada.js

--- bus/static/scripts/parada.js
+++ bus/static/scripts/parada.js
@@ -11,6 +11,46 @@ setInterval(function() { actualizar(last) }, 30000);    // Actualizar los datos 
 var last = new Date();
 document.getElementById('boton-recarga').style.display = "";    // Mostrar el botón de actualización
 
+function mostrar_lineas(obj) {
+    document.getElementById('lineas').innerHTML = '';   // Vaciar el contenedor
+
+    // Iterar sobre las líneas
+    for(i=0; i<obj.length; i++) {
+        // Nombre de la línea y origen en caso de que su nombre sea UDC
+        if( obj[i].linea['nombre'] == 'UDC') { cierre = ' <small style="font-size: 60%">(origen: '+obj[i].linea['origen']+')</small></h1>'; } else { cierre = '</h1>'; }
+        document.getElementById('lineas').innerHTML += '<h1>'+cadenas[idioma][0]+' <span class="simbolo_linea" style="background-color: #'+obj[i].linea['color']+'">'+obj[i].linea['nombre']+'<span>'+cierre;
+
+        // Mostrar los buses para la línea
+        buses = obj[i].buses
+        for(j=0; j<buses.length; j++) {
+            // Formatear la distancia
+            if(buses[j].distancia >= 1000) {
+                distancia = 'a '+Number(buses[j].distancia/1000).toFixed(1)+'km, ';
+            }
+            else {
+                distancia = 'a '+buses[j].distancia+'m, ';
+            }
+
+            // Si el bus está a más de una hora, calcular la hora de llegada
+            if(buses[j].tiempo >= 60) {
+                ahora = new Date();
+                ahora.setMinutes(ahora.getMinutes()+parseInt(buses[j].tiempo, 10));
+                tiempo = ahora.getHours()+':'+(ahora.getMinutes()<10?'0':'')+ahora.getMinutes()+'.'
+            }
+            else if(buses[j].tiempo != 0) {
+                tiempo = buses[j].tiempo+'\'.';
+            }
+            else {
+                tiempo = '<b>'+cadenas[idioma][1]+'</b>.';
+                distancia = '';
+            }
+            bus = '<p>Bus '+buses[j].bus+', '+distancia+tiempo;
+            document.getElementById('lineas').innerHTML += bus;
+        }
+    }
+    document.getElementById('t').innerHTML = cadenas[idioma][3]+': '+now.toLocaleString(cadenas[idioma][4]);
+}
+
 function actualizar(last) {
     var now = new Date();
 
@@ -25,33 +65,7 @@ function actualizar(last) {
         .then(function(response) { return response.json(); })
         .then(function(json) {
             const obj = json['buses']['lineas'];   // Almacenar los datos en una variable
-            document.getElementById('lineas').innerHTML = '';   // Vaciar el contenedor
-
-            // Iterar sobre las líneas
-            for(i=0; i<obj.length; i++) {
-                titulo = '<h1>'+cadenas[idioma][0]+' <span class="simbolo_linea" style="background-color: #'+obj[i].linea['color']+'">'+obj[i].linea['nombre']+'</span>'
-                if(obj[i].linea['nombre'] == 'UDC') { titulo += ' <small style="font-size: 60%">(origen: '+obj[i].linea['origen']+')</small></h1>'; } else { titulo += '</h1>' }
-                document.getElementById('lineas').innerHTML += titulo;
-                for(b=0; b<obj[i].buses.length; b++) {
-                    if(obj[i].buses[b].tiempo >= 60) {
-                        ahora = new Date();
-                        ahora.setMinutes(ahora.getMinutes()+parseInt(obj[i].buses[b].tiempo, 10));
-                        hora = ahora.getHours()+':'+(ahora.getMinutes()<10?'0':'')+ahora.getMinutes();
-                        if (obj[i].buses[b].distancia >= 1000) { distancia = Number(obj[i].buses[b].distancia/1000).toFixed(1); u='km'} else { distancia = obj[i].buses[b].distancia; u='m'}
-                        linea = '<p>Bus '+obj[i].buses[b].bus+', a '+distancia+u+', '+hora+'.</p>';
-                    }
-                    else if(obj[i].buses[b].tiempo != 0) {
-                        hora = obj[i].buses[b].tiempo;
-                        if (obj[i].buses[b].distancia >= 1000) { distancia = Number(obj[i].buses[b].distancia/1000).toFixed(1); u='km'} else { distancia = obj[i].buses[b].distancia; u='m'}
-                        linea = '<p>Bus '+obj[i].buses[b].bus+', a '+distancia+u+', '+hora+'\'.</p>';
-                    }
-                    else {
-                        linea = '<p>Bus '+obj[i].buses[b].bus+', <b>'+cadenas[idioma][1]+'</b>.';
-                    }
-                    document.getElementById('lineas').innerHTML += linea;
-                }
-            }
-            document.getElementById('t').innerHTML = cadenas[idioma][3]+': '+now.toLocaleString(cadenas[idioma][4]);
+            mostrar_lineas(obj);
         });
     }
     else { flash_error(cadenas[idioma][2]); }
